66f14bc96dfa41d08a92bf09c4e2dd77cf5c92db
bacchusjazz <bacchusjazz6248@gmail.com>
2020-04-29 06:12:02 +0900

    InfoPlusScene 学習者情報追加フォーム実装

---
+++ Assets/Scripts/InfoPlus/InputSystem.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System.IO;
+using UnityEngine.SceneManagement;
+
+public class InputSystem : MonoBehaviour {
+    
+    public InputField inputField;
+    public Text text;
+    private int i = 0;
+
+    void Start () {
+        inputField = inputField.GetComponent<InputField> ();
+        text = text.GetComponent<Text>();
+    }
+
+    public void InputText(){
+        text.text = inputField.text;
+    }
+
+    public void OnClickRegistration()
+    {
+        if (i == 0)
+        {
+            StreamWriter sw = new StreamWriter("../InfoData.txt",false);
+            sw.WriteLine(text.text);
+            sw.Flush();
+            sw.Close();
+            i++;
+            InputField form = GameObject.Find("InputField").GetComponent<InputField>();
+            form.text = "";
+        }
+        else
+        {
+            StreamWriter sw = new StreamWriter("../InfoData.txt", append: true);
+            sw.WriteLine(text.text);
+            sw.Flush();
+            sw.Close();
+            InputField form = GameObject.Find("InputField").GetComponent<InputField>();
+            form.text = "";
+        }
+    }
+
+    public void OnClickNext()
+    {
+        SceneManager.LoadScene("ColumnScene");
+    }
+}
