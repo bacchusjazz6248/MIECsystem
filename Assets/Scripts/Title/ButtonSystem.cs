7b965a12c4101e6882b7b6e89f07fa729ece2b86
bacchusjazz <bacchusjazz6248@gmail.com>
2020-04-29 03:35:04 +0900

    タイトルシーン ボタン実装

---
+++ Assets/Scripts/Title/ButtonSystem.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonSystem : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject SB = GameObject.Find("StartButton");
+        GameObject CB = GameObject.Find("ContinueButton");
+        GameObject EB = GameObject.Find("ExitButton");
+    }
+
+    // Update is called once per frame
+    public void OnClickSB()
+    {
+        SceneManager.LoadScene("InfoPlusScene");
+    } 
+    
+    public void OnClickCB()
+    {
+        SceneManager.LoadScene("ConfirmScene");
+    } 
+    
+    public void OnClickEB()
+    {
+        UnityEngine.Application.Quit();
+    } 
+}
